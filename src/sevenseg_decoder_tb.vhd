d499b7750d10fdedb41d6287d523a7a62887bfaf
jayblazen <70984269+jayblazen@users.noreply.github.com>
2026-05-06 12:56:34 -0600

    changed seveTb

--- src/sevenseg_decoder_tb.vhd
+++ src/sevenseg_decoder_tb.vhd
@@ -1,64 +1,46 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 02/24/2026 08:06:50 AM
--- Design Name: 
--- Module Name: sevenseg_decoder - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity sevenseg_decoder_tb is
-    Port ( i_Hex : in STD_LOGIC_VECTOR (3 downto 0);
-           o_seg_n : out STD_LOGIC_VECTOR (6 downto 0));
 end sevenseg_decoder_tb;
 
 architecture Behavioral of sevenseg_decoder_tb is
 
+    -- Signals to connect to DUT
+    signal i_Hex   : std_logic_vector(3 downto 0);
+    signal o_seg_n : std_logic_vector(6 downto 0);
+
 begin
-    with i_Hex select
-        o_seg_n <=
-            "1000000" when "0000", -- 0
-            "1111001" when "0001", -- 1
-            "0100100" when "0010", -- 2
-            "0110000" when "0011", -- 3
-            "0011001" when "0100", -- 4
-            "0010010" when "0101", -- 5
-            "0000010" when "0110", -- 6
-            "1111000" when "0111", -- 7
-            "0000000" when "1000", -- 8
-            "0010000" when "1001", -- 9
-            "0001000" when "1010", -- A
-            "0000011" when "1011", -- b
-            "1000110" when "1100", -- C
-            "0100001" when "1101", -- d
-            "0000110" when "1110", -- E
-            "0001110" when "1111", -- F
-            "1111111" when others;
 
+    -- Instantiate DUT
+    uut: entity work.sevenseg_decoder
+        port map (
+            i_Hex   => i_Hex,
+            o_seg_n => o_seg_n
+        );
 
+    -- Stimulus process
+    stim_proc: process
+    begin
+        -- Test all values 0 → F
+        i_Hex <= "0000"; wait for 10 ns;
+        i_Hex <= "0001"; wait for 10 ns;
+        i_Hex <= "0010"; wait for 10 ns;
+        i_Hex <= "0011"; wait for 10 ns;
+        i_Hex <= "0100"; wait for 10 ns;
+        i_Hex <= "0101"; wait for 10 ns;
+        i_Hex <= "0110"; wait for 10 ns;
+        i_Hex <= "0111"; wait for 10 ns;
+        i_Hex <= "1000"; wait for 10 ns;
+        i_Hex <= "1001"; wait for 10 ns;
+        i_Hex <= "1010"; wait for 10 ns;
+        i_Hex <= "1011"; wait for 10 ns;
+        i_Hex <= "1100"; wait for 10 ns;
+        i_Hex <= "1101"; wait for 10 ns;
+        i_Hex <= "1110"; wait for 10 ns;
+        i_Hex <= "1111"; wait for 10 ns;
+
+        wait; -- stop simulation
+    end process;
 
 end Behavioral;
